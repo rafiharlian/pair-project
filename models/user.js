8e8947aa9858376733ce4eb8ef95da256acff0ad
rafi <rafiharlian@gmail.com>
2020-06-24 17:56:18 +0700

    ndang

---
+++ models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const { Model } = sequelize.Sequelize
+  class User extends Model {}
+  User.init({
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  },{
+    sequelize
+  });
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
