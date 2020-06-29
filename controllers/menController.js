5968978c24d825572903558453f4330ff97afed6
rafi <rafiharlian@gmail.com>
2020-06-29 20:35:56 +0700

    go for it

--- controllers/menController.js
+++ controllers/menController.js
@@ -1,11 +1,20 @@
-const { Men } = require('../models')
+const { Product } = require('../models')
 
 class menController{
     static menpage(req, res){
         res.render('men')
         
     }
-
+    static menread(req, res){
+        Product.findAll()
+        .then(result => {
+            console.log(result)
+            res.render('men', {result})
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    }
 }
 
 module.exports = menController
--- routes/men.js
+++ routes/men.js
@@ -3,6 +3,7 @@ const menController = require('../controllers/menController')
 const { Router } = require('express')
 
 router.get('/', menController.menpage)
+router.get('/menproduct', menController.menread)
 
 
 module.exports = router
